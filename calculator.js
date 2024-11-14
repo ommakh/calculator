f6d4d8a482707a725d309c1ea7c41fc10424230d
OM MAKH <94157617+ommakh@users.noreply.github.com>
2024-11-14 19:05:55 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,25 @@
+function calculate() {
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    const operation = document.getElementById('operation').value;
+
+    let result;
+    switch (operation) {
+      case 'add':
+        result = num1 + num2;
+        break;
+      case 'subtract':
+        result = num1 - num2;
+        break;
+      case 'multiply':
+        result = num1 * num2;
+        break;
+      case 'divide':
+        result = num2 !== 0 ? num1 / num2 : 'Error: Division by zero';
+        break;
+      default:
+        result = 'Invalid operation';
+    }
+
+    document.getElementById('result').value = result;
+  }
